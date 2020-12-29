8ce785d6b43b5e27035b869a88907ff6f4da58b8
StanislavSorochak <75738611+StanislavSorochak@users.noreply.github.com>
2020-12-29 14:00:14 +0200

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba12.2.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Spusok* first = NULL;
+			Spusok* last = NULL;
+			for (int i = 0; i < 2; i++)
+				Create(first, last, i);
+
+			Spusok* T = first;
+			int x = 5;
+			Process(T, x);
+
+			T = first;
+			int l = T->inf;
+			Assert::AreEqual(l, 5);
+		}
+	};
+}
